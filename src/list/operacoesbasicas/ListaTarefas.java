0db6a2d73aaed9ea344a6559f4252bfa34fd110d
VivianKailany <viviankailanymarques@gmail.com>
2024-05-16 20:19:51 -0300

    operacoes simples com list

---
+++ src/list/operacoesbasicas/ListaTarefas.java
@@ -0,0 +1,47 @@
+package list.operacoesbasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefas {
+
+    private List<Tarefa> tarefaList;
+
+    public ListaTarefas() {
+        this.tarefaList = new ArrayList<>();
+    }
+    
+    public void adicionarTarefa(String descricao){
+        tarefaList.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao){
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+        for (Tarefa t: tarefaList){
+            if(t.getDescricao().equalsIgnoreCase(descricao)){
+                tarefasParaRemover.add(t);
+            }
+        }
+        tarefaList.removeAll(tarefasParaRemover);
+    }
+
+    public int obterNumeroTotalTarefas(){
+        return tarefaList.size();
+    }
+
+    public void obterDescricoesTarefas(){
+        System.out.println(tarefaList);
+    }
+
+    public static void main(String[] args) {
+        ListaTarefas listaTarefas = new ListaTarefas();
+
+        listaTarefas.adicionarTarefa("Comer arroz");
+        listaTarefas.adicionarTarefa("Comer feijao");
+        System.out.println(listaTarefas.obterNumeroTotalTarefas());
+        listaTarefas.obterDescricoesTarefas();
+        
+
+    }
+
+}
